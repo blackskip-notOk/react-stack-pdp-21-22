cecbf7b9b9cec89df9b00c3d173d52c1187da446
Ilya Babanov <ilbabanov@gmail.com>
2022-02-21 21:25:16 +0400

    21.02.2022 add Jest and App.test

---
+++ src/__tests__/App.test.tsx
@@ -0,0 +1,9 @@
+import { App } from '../App';
+import { render } from '@testing-library/react';
+import { BrowserRouter } from 'react-router-dom';
+
+test('Renders main page correctly', () => {
+    render(<BrowserRouter><App /></BrowserRouter>);
+
+  expect(true).toBeTruthy();
+});
