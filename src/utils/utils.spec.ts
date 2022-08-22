00801b7e7e12eb1e4dba03b37814c58a0ac988a6
Ilya Babanov <Ilya.Babanov@magenta-technology.com>
2022-08-22 11:38:15 +0400

    add utils tests

--- src/utils/utils.spec.ts
+++ src/utils/utils.spec.ts
@@ -1,4 +1,10 @@
-import { idGenerator } from '~/utils';
+import {
+	clearObject,
+	getFriendParam,
+	getSearchParamsFromUrl,
+	idGenerator,
+	searchParamsSerializer,
+} from '~/utils';
 import { FollowResponse } from '~/store/slices/usersSlice/types';
 import { Description, ServerMessage } from '~/constants/serverMessages';
 import { getFollowResult } from '.';
@@ -115,6 +121,146 @@ describe('app utils functions', () => {
 			it.next();
 
 			expect(it.next().value).toBe(7);
+		});
+	});
+
+	describe('clearObject', () => {
+		const noNullObj = {
+			page: '2',
+			count: '25',
+			term: 'test user',
+			friend: '1',
+		};
+
+		const nullObj = {
+			page: null,
+			count: '25',
+			term: null,
+			friend: '1',
+		};
+
+		const cleanObj = {
+			count: '25',
+			friend: '1',
+		};
+
+		it('if object does not have field with null value, should return the same object', () => {
+			expect(clearObject(noNullObj)).toStrictEqual(noNullObj);
+		});
+
+		it('if object have field with null value, should return clean object', () => {
+			expect(clearObject(nullObj)).toStrictEqual(cleanObj);
+		});
+
+		it('if object have only fields with null value, should return null', () => {
+			expect(clearObject({ page: null, count: null, term: null, friend: null })).toBeNull();
+		});
+	});
+
+	describe('searchParamsSerializer', () => {
+		it('if does not set params, should return null', () => {
+			expect(searchParamsSerializer({ page: 1, count: 10 })).toBeNull();
+		});
+
+		it('if set params, should return correct object', () => {
+			expect(searchParamsSerializer({ page: 2, count: 100 })).toStrictEqual({
+				page: '2',
+				count: '100',
+			});
+			expect(searchParamsSerializer({ page: 1, count: 25 })).toStrictEqual({ count: '25' });
+			expect(searchParamsSerializer({ page: 2, count: 10 })).toStrictEqual({ page: '2' });
+			expect(searchParamsSerializer({ page: 2, count: 100 })).toStrictEqual({
+				page: '2',
+				count: '100',
+			});
+			expect(searchParamsSerializer({ page: 1, count: 10, term: 'test user' })).toStrictEqual({
+				term: 'test user',
+			});
+			expect(searchParamsSerializer({ page: 2, count: 10, term: 'test user' })).toStrictEqual({
+				page: '2',
+				term: 'test user',
+			});
+			expect(searchParamsSerializer({ page: 1, count: 25, term: 'test user' })).toStrictEqual({
+				count: '25',
+				term: 'test user',
+			});
+			expect(
+				searchParamsSerializer({ page: 1, count: 10, term: 'test user', friend: true }),
+			).toStrictEqual({
+				term: 'test user',
+				friend: '1',
+			});
+			expect(
+				searchParamsSerializer({ page: 1, count: 10, term: 'test user', friend: false }),
+			).toStrictEqual({
+				term: 'test user',
+				friend: '0',
+			});
+			expect(
+				searchParamsSerializer({ page: 2, count: 25, term: 'test user', friend: false }),
+			).toStrictEqual({
+				page: '2',
+				count: '25',
+				term: 'test user',
+				friend: '0',
+			});
+		});
+	});
+
+	describe('getSearchParamsFromUrl', () => {
+		it('if empty paarams, should return null', () => {
+			expect(getSearchParamsFromUrl({})).toBeNull();
+		});
+
+		it('if have params in url, should return correct object', () => {
+			expect(
+				getSearchParamsFromUrl({
+					count: '2',
+					page: '25',
+					term: 'test user',
+					friend: '0',
+				}),
+			).toStrictEqual({
+				count: 2,
+				page: 25,
+				term: 'test user',
+				friend: false,
+			});
+
+			expect(
+				getSearchParamsFromUrl({
+					term: 'test user',
+					friend: '0',
+				}),
+			).toStrictEqual({
+				count: undefined,
+				page: undefined,
+				term: 'test user',
+				friend: false,
+			});
+
+			expect(
+				getSearchParamsFromUrl({
+					count: '2',
+					page: '25',
+					friend: '1',
+				}),
+			).toStrictEqual({
+				count: 2,
+				page: 25,
+				friend: true,
+				term: undefined,
+			});
+		});
+	});
+
+	describe('getFriendParam', () => {
+		it('should return boolean or undefined', () => {
+			expect(getFriendParam('all')).toBeUndefined();
+			expect(getFriendParam('friends')).toBeDefined();
+			expect(getFriendParam('friends')).toBeTruthy();
+			expect(getFriendParam('notfriends')).toBeDefined();
+			expect(getFriendParam('notfriends')).toBeFalsy();
 		});
 	});
 });
